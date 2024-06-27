747346223f81b66abf93475b542c9f0bba833045
lucasdiasbrr <lucas.dias.borro@gmail.com>
2024-06-26 23:21:42 -0300

    OUTRO PROJETO BASICAMENTE PARA O EXAME

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './src/pages/Login';
-import Cadastro from './src/pages/Cadastro';
-import Home from './src/pages/Home';
-import ItemsCadastrados from './src/pages/ItemsCadastrados'; 
-
-const Stack = createNativeStackNavigator();
+import Navigation from './Natigation';
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ItemsCadastrados" component={ItemsCadastrados} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+  return <Navigation />;
+};
 
 export default App;
